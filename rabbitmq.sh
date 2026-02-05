9e661eb4f9470708687c0b11249e51971f929923
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-05 14:06:08 +0530

    initial commit of rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# common.sh file is sourced to use the common functions and variables defined in it.
+source ./common.sh
+
+# checking for root user access.
+check_root
+# installing rabbitmq.
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added RabbitMQ repo"
+
+dnf install rabbitmq-server -y &>>$LOGS_FILE
+VALIDATE $? "Installing RabbitMQ server"
+
+systemctl enable rabbitmq-server &>>$LOGS_FILE
+systemctl start rabbitmq-server
+VALIDATE $? "Enabled and started rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
+VALIDATE $? "created user and gien permissions"
+
+print_total_time
