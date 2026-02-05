ec9f7240aa89c295d39eec34e8a1871aabf9e25d
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-05 14:09:56 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # common.sh file is sourced to use the common functions and variables defined in it.
 source ./common.sh
-app_name=catalogue
+app_name=shipping
 # checking for root user access.
 check_root
 app_setup
