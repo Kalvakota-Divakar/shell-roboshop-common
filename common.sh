b3bfc31281b1b9a9531917aa3cc05a5d72b33eac
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-05 11:54:42 +0530

    initial commit

---
+++ common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Validate whether the root user is executing the script or not.
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log" # /var/log/shell-roboshop/mongodb.sh.log
+# color codes given for outpit messages.
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+START_TIME=$(date +%s)
+#date and time of script execution.
+echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
+# check for root user.
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+    fi
+}
+mkdir -p $LOGS_FOLDER # create log folder if not exists.
+# function to validate each command execution status.
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE # append the log file.
+        exit 1 # exit without failure status.
+    else
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL-TIME=$((END_TIME-START_TIME))
+    echo -e  "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+}
+
---
+++ mongodb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# common.sh file is sourced to use the common functions and variables defined in it.
+source ./common.sh
+
+# checking for root user access.
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo" 
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
+# allowing remote connections to mongodb by changing the bindIp value in mongod.conf file.
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
+
+print_total_time 
