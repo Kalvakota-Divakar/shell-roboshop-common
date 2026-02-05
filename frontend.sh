c4c7d3834e7c367893563adfa762f6c7b54cb814
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-05 14:28:39 +0530

    actions

---
+++ frontend.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# common.sh file is sourced to use the common functions and variables defined in it.
+source ./common.sh
+
+# checking for root user access.
+check_root
+# Setting up Nginx Repository.
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx  &>>$LOGS_FILE
+systemctl start nginx 
+VALIDATE $? "Enabled and started nginx"
+# removing default content and downloading the frontend content and unzipping it to nginx html directory.
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Remove default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and unzipped frontend"
+# removing default nginx.conf file and copying our nginx.conf file to nginx directory.
+rm -rf /etc/nginx/nginx.conf
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied our nginx conf file"
+
+systemctl restart nginx
+VALIDATE $? "Restarted Nginx"
+
+print_total_time
