ac9299be7c57173a8f33a4333bd468ac56e28bcf
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-05 13:30:36 +0530

    initial commit of mysql

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# common.sh file is sourced to use the common functions and variables defined in it.
+source ./common.sh
+
+# checking for root user access.
+check_root
+# installing mysql.
+echo "Setting up MySQL Repository.
+dnf install mysql-server -y &>>$LOGS_FILE
+VALIDATE $? "Install MySQL server"
+
+systemctl enable mysqld &>>$LOGS_FILE
+systemctl start mysqld  
+VALIDATE $? "Enable and start mysql"
+
+# get the password from user
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setup root password"
+
+print_total_time 
